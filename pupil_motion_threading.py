fc561e102b06dfe5ca1e0725aad261b33f9aee84
wehrlab <yavorska@uoregon.edu>
2018-03-01 15:36:17 -0800

    fixing

--- pupil_motion_threading.py
+++ pupil_motion_threading.py
@@ -35,7 +35,46 @@ global movements
 delay = .1 #change duty cycle every 100 ms
 mouse = devices.mice[0]
 channel = 15
+GPIO.setup(channel, GPIO.OUT)
+p = GPIO.PWM(channel,100)
+p.start(50)
+all_move_local = []
 
+class pi_motion(threading.Thread):
+	
+	def baseline():
+		p.ChangeDutyCycle(50)
+
+
+	def update_pin():
+		if movements:
+			move_local = movements
+			movements = []
+			move_local = np.divide(move_local, 1.5)
+			move_local = np.clip(move_local, -50, 50)
+			move_local = move_local.mean(dtype=np.int)
+			move_local +=50
+			print(move_local)
+			
+			p.ChangeDutyCycle(100.0 - move_local)
+			Timer(delay, update_pin).start()
+		else:
+			baseline()
+			Timer(delay, update_pin).start()
+	
+	movements = []
+	update_pin()
+	while 1:
+		try:
+			events=mouse.read()
+			movements.extend([event.state for event in events if event.code == "REL_Y"])
+		except KeyboardInterrupt:
+			p.stop()
+			GPIO.cleanup()
+			time_string= strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+			name= "".join([time_string, '.txt'])
+			with open(name, 'w') as f:
+				json.dump(all_move_local, f, ensure_ascii=False)
 
 class pi_video(threading.Thread):
 	def start_listener(on_pin, off_pin, SCT_pin):
@@ -113,50 +152,7 @@ class pi_video(threading.Thread):
 	            
 	            	SCT=[]
 
-class pi_motion(threading.Thread):
-	
-	
-	GPIO.setup(channel, GPIO.OUT)
-	p = GPIO.PWM(channel,100)
-	p.start(50)
-	all_move_local = []
-	print('Running movements')
 
-		
-
-	def baseline():
-		p.ChangeDutyCycle(50)
-
-
-	def update_pin():
-		if movements:
-			move_local = movements
-			movements = []
-			move_local = np.divide(move_local, 1.5)
-			move_local = np.clip(move_local, -50, 50)
-			move_local = move_local.mean(dtype=np.int)
-			move_local +=50
-			print(move_local)
-			
-			p.ChangeDutyCycle(100.0 - move_local)
-			Timer(delay, update_pin).start()
-		else:
-			baseline()
-			Timer(delay, update_pin).start()
-	
-	movements = []
-	update_pin()
-while 1:
-	try:
-		events=mouse.read()
-		movements.extend([event.state for event in events if event.code == "REL_Y"])
-	except KeyboardInterrupt:
-		p.stop()
-		GPIO.cleanup()
-		time_string= strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
-		name= "".join([time_string, '.txt'])
-		with open(name, 'w') as f:
-			json.dump(all_move_local, f, ensure_ascii=False)
 
 #Create Class
 motion = pi_motion()
